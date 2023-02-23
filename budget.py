c3e6d8028948c17bd0998720ae2c0d821e634e3a
owningoffame <59062554+owningoffame@users.noreply.github.com>
2023-02-23 17:52:44 +0400

    Update budget.py

--- budget.py
+++ budget.py
@@ -28,113 +28,172 @@ df = df[~df['Category'].isin(filtered_categories)]
 df = df.groupby(['Category']).agg({
     'Payment_Amount': 'sum'}).reset_index()
 
-# Create Cost DataFrame
-cost_df = df[df['Payment_Amount'] <= 0].sort_values('Payment_Amount', ascending=True)
-cost_df['Amount'] = abs(df['Payment_Amount'])  # For Sunburst chart
+# Create Expenses DataFrame
+expenses_df = df[df['Payment_Amount'] <= 0].sort_values('Payment_Amount', ascending=True)
+expenses_df['Amount'] = abs(df['Payment_Amount'])  # For Sunburst chart
 
 # Grope by macro-categories
 home_categories = ['ЖКХ',
                    'Дом'
                    ]
 food_categories = ['Супермаркеты',
-                   'Фастфуд',
                    'Кофе',
                    'Рестораны'
                    ]
 health_categories = ['Аптеки,'
                      'Красота и здоровье',
                      'Медицина',
-                     'Мед. услуги'
-
+                     'Мед. услуги',
+                     'Аптеки',
+                     'Красота и здоровье'
                      ]
 transport_categories = ['Транспорт',
                         'Автобусы',
                         'Такси',
                         'Авиабилеты'
                         ]
+longtermbyus_categories = ['Техника',
+                           'Одежда и обувь'
+                           ]
+education_categories = ['Книги',
+                        'Образование'
+                        ]
+investments_categories = ['Инвестиции'
 
-cost_df['Group'] = select(
+                          ]
+garbage_categories = ['Алкоголь, табак',
+                      'Фастфуд'
+                      ]
+internet_categories = ['Мобильная связь',
+                       'Домашний интернет',
+                       'Подписки'
+
+                       ]
+presents_categories = ['Подарки',
+                       'Цветы',
+                       'Семья'
+                       ]
+onetimebyus_categories = ['Госуслуги',
+                          'Юридические услуги',
+                          ]
+credits_categories = ['Кредитка',
+                      'Займы'
+                      ]
+
+expenses_df['Group'] = select(
     [
-        cost_df['Category'].isin(home_categories),
-        cost_df['Category'].isin(food_categories),
-        cost_df['Category'].isin(health_categories),
-        cost_df['Category'].isin(transport_categories)
+        expenses_df['Category'].isin(home_categories),
+        expenses_df['Category'].isin(food_categories),
+        expenses_df['Category'].isin(health_categories),
+        expenses_df['Category'].isin(transport_categories),
+        expenses_df['Category'].isin(longtermbyus_categories),
+        expenses_df['Category'].isin(education_categories),
+        expenses_df['Category'].isin(investments_categories),
+        expenses_df['Category'].isin(garbage_categories),
+        expenses_df['Category'].isin(internet_categories),
+        expenses_df['Category'].isin(presents_categories),
+        expenses_df['Category'].isin(onetimebyus_categories),
+        expenses_df['Category'].isin(credits_categories)
     ],
     [
         'Home',
         'Food',
-        'Health',
-        'Transport'
+        'Selfcare and Health',
+        'Transport',
+        'Long-term Buys',
+        'Education and Seft-dev',
+        'Investments',
+        'Garbage',
+        'Internet',
+        'Presents',
+        'One-time Buys',
+        'Credits'
     ],
     default='Unknown'
 )
 
 # Print df to check
-print(cost_df)
+print(expenses_df)
 
 # Create charts
 table = go.Figure(data=[go.Table(
     header=dict(
-        values=['Category', 'Cost'],
+        values=['Category', 'Expenses'],
         fill_color='lavender',
         align='left', font=dict(color='black', size=12)
     ),
     cells=dict(
-        values=[cost_df.Category, cost_df.Payment_Amount],
+        values=[expenses_df.Category, expenses_df.Payment_Amount],
         align='left', font=dict(color='black', size=11)
     )
 )])
 
-bar = go.Figure(data=go.Bar(
-    name='Cost',
-    x=cost_df.Payment_Amount,
-    y=cost_df.Category,
+bar = go.Figure(go.Bar(
+    name='Expenses',
+    x=expenses_df.Payment_Amount,
+    y=expenses_df.Category,
     orientation='h')
 )
 
 sunburst = px.sunburst(
-    cost_df,
+    expenses_df,
     path=['Group', 'Category'],
-    values='Amount')
+    values='Amount'
+)
+
+total = go.Figure(go.Indicator(
+    mode="number",
+    value=sum(expenses_df.Payment_Amount))
+)
 
 # Update charts
 sunburst.update_layout(margin={'l': 0, 'b': 0, 't': 0, 'r': 0})
-table.update_layout(margin={'l': 20, 'b': 0, 't': 0, 'r': 0})
+table.update_layout(margin={'l': 20, 'b': 0, 't': 0, 'r': 0},
+                    height=1000)
 bar.update_layout(margin={'l': 20, 'b': 20, 't': 30, 'r': 20},
-                  barmode='stack',
                   yaxis={'categoryorder': 'total descending'})
+total.update_layout(margin={'l': 0, 'b': 0, 't': 0, 'r': 0})
 
 # Create Dash
 app = Dash(__name__)
-app.layout = html.Div(children=[
-    # All elements from the top of the page
+app.layout = html.Div([
+    html.H1('Budget Board'),
+
     html.Div([
+        html.H2('Expense Categories'),
+        dcc.Graph(
+            id='table',
+            figure=table
+        ),
+    ],  style={'width': '25%', 'float': 'left', 'display': 'inline-block'}
+    ),
 
+    html.Div([
         html.Div([
-            html.H1(children='Cost Category'),
+            html.H2('Total'),
             dcc.Graph(
-                id='graph1',
-                figure=table
-            ),
-        ], style={'width': '25%', 'float': 'left', 'display': 'inline-block'}),
+                id='total',
+                figure=total,
+                style={'height': '30px'}
+            )
+        ], className='wrap'),
+        dcc.Graph(
+            id='bar',
+            figure=bar
+        ),
+    ],  style={'width': '23%', 'display': 'inline-block'}
+    ),
 
-        html.Div([
-            html.H1(children='"'),
-            dcc.Graph(
-                id='graph2',
-                figure=bar
-            ),
-        ], style={'width': '25%', 'display': 'inline-block'}),
+    html.Div([
+        html.H2('Groups'),
+        dcc.Graph(
+            id='sunburst',
+            figure=sunburst
+        ),
+    ], style={'width': '50%', 'float': 'right', 'display': 'inline-block'}
+    ),
 
-        html.Div([
-            html.H1(children='Groups'),
-            dcc.Graph(
-                id='graph3',
-                figure=sunburst
-            ),
-        ], style={'width': '50%', 'float': 'right', 'display': 'inline-block'}),
-    ]),
-])
+], style={'min-height': '100vh'})
 
 if __name__ == '__main__':
     app.run_server(debug=True,
